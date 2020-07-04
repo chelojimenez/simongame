7e2e0d0b6306b6b6359f7f88fa0eac3c19ccf3e9
marcelojimenezr <49251563+marcelojimenezr@users.noreply.github.com>
2020-07-04 13:54:42 -0400

    added randomness

--- game.js
+++ game.js
@@ -3,12 +3,20 @@ gamePatern = [];
 
 randomChosenColor = bottonColors[nextSequence()];
 
+console.log(randomChosenColor);
+
 gamePatern.push(randomChosenColor);
+
+var button = $("#" + randomChosenColor).select();
+
+// animate it
+button.fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 
 
 
 function nextSequence()
 {
-    var randomNumber = Math.floor(Math.random() * 3) + 1;
+    var randomNumber = (Math.floor(Math.random() * 4));
+    console.log(randomNumber);
     return randomNumber;
 }
